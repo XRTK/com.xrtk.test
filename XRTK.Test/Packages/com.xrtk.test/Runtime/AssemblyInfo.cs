c48ebe0167c24c2bbfdf74344f329aa35e52d53a
Stephen Hodgson <hodgson.designs@gmail.com>
2022-11-01 20:40:33 -0400

    updated project to 2021.3.12f1 LTS

--- XRTK.Test/Packages/com.xrtk.test/Runtime/AssemblyInfo.cs
+++ XRTK.Test/Packages/com.xrtk.test/Runtime/AssemblyInfo.cs
@@ -5,7 +5,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.2")]
 [assembly: AssemblyTitle("com.xrtk.test")]
 [assembly: AssemblyCompany("XRTK")]
 [assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
